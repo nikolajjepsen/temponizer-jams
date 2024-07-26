34a73b27d3112d879413627903de4207c1398436
nikolajjepsen <nj@codefighter.dk>
2024-07-26 14:22:14 +0200

    vite target

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/temponizer-jams/"
+    build: {
+        target: 'esnext'
+    },
+    base: "/temponizer-jams/",
 })
